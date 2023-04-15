5a9d6255c8eeb6565beb51f7fd39d5cc70a0e2d5
Saif Maamun <sadifmamun@gmail.com>
2023-04-15 13:31:34 +0600

    hero text done

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -11,9 +11,22 @@ const Banner = () => {
         <div className="carousel w-full">
   <div id="slide1" className="carousel-item relative w-full">
     <img alt=""src={img1} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide6" className="btn btn-circle">❮</a> 
+    <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+      <a href="#slide6" className="btn btn-circle mr-5">❮</a> 
       <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+    {/* hero title */}
+    <div className="absolute flex justify-start transform -translate-y-1/2 left-24 top-1/4">
+      <h1 className='text-6xl font-bold text-white'>Affordable <br /> Price For Car <br /> Servicing</h1>
+    </div>
+    {/* hero subtitle */}
+    <div className="w-2/5 absolute flex justify-start transform -translate-y-1/2 left-24 top-1/2">
+      <p className='text-xl text-white'>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+    </div>
+    {/* hero button */}
+    <div className="absolute flex justify-start transform -translate-y-1/2 left-24 top-3/4">
+    <button className="btn btn-active btn-secondary mr-5">Button</button>
+    <button className="btn btn-outline btn-secondary">Button</button>
     </div>
   </div> 
   <div id="slide2" className="carousel-item relative w-full">
